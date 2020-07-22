f61302f737d9fbde549df12bec7ac20a921b6a14
aditismalagi <66186744+aditismalagi@users.noreply.github.com>
2020-07-22 16:59:25 -0400

    Create Solution 7.sh

---
+++ Shell Scripting Practice by Adu/Shell Scripting Succinctly/Solution 7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=$1
+if [ -f $FILE ]
+then
+	echo "$FILE is a regular file."
+elif [ -d $FILE ]
+then
+	echo "$FILE is a directory."
+else
+	echo "$FILE is some other type of file."
+fi
+ls -l $FILE
